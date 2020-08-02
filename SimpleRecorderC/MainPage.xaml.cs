8cfe1c6fc5879a49da3269ce81b2f66a6f2dd20f
YAMAMI Taro <mitarothanken@gmail.com>
2020-08-02 21:30:17 +0900

    見た目を元プロジェクトから持ってきてみる
    すぐに解決しないところはコメントアウト

--- SimpleRecorderC/MainPage.xaml.cs
+++ SimpleRecorderC/MainPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,18 +10,27 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
 
 namespace SimpleRecorderC
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainPage.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
